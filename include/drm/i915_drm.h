32c7b3bb672cccc7fdf09bd816fbb69fd0fa7be1
Sudheer Gurram <sudheer.gurram@intel.com>
2013-07-02 09:05:35 +0530

    FOR_UPSTREAM [VPG]: Added IOCTL for DPST Feature
    BZ: 119407
    Added new IOCTL for DPST feature
    
    Change-Id: I92e0fa107fa3bd0695e597eac985fa05374ee709
    Signed-off-by: Sudheer Gurram <sudheer.gurram@intel.com>
    Reviewed-on: http://android.intel.com:8080/115759
    Reviewed-by: Satyanantha, Rama Gopal M <rama.gopal.m.satyanantha@intel.com>
    Reviewed-by: Kumar, Shobhit <shobhit.kumar@intel.com>
    Tested-by: Sun, Hang L <hang.l.sun@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>
    
    Conflicts:
    
    	include/drm/i915_drm.h

--- include/drm/i915_drm.h
+++ include/drm/i915_drm.h
@@ -263,6 +263,7 @@ typedef struct _drm_i915_sarea {
 #define DRM_I915_GEM_USERPTR		0x33
 #define DRM_I915_RESERVED_REG_BIT_2	0x37
 #define DRM_I915_SET_CSC                0x39
+#define DRM_I915_DPST_CONTEXT		0x3b
 #define DRM_I915_GEM_ACCESS_USERDATA	0x3c
 
 #define DRM_IOCTL_I915_INIT		DRM_IOW( DRM_COMMAND_BASE + DRM_I915_INIT, drm_i915_init_t)
@@ -323,6 +324,9 @@ typedef struct _drm_i915_sarea {
 #define DRM_IOCTL_I915_GEM_ACCESS_USERDATA	\
 		DRM_IOWR(DRM_COMMAND_BASE + DRM_I915_GEM_ACCESS_USERDATA, \
 			struct drm_i915_gem_access_userdata)
+#define DRM_IOCTL_I915_DPST_CONTEXT			\
+		DRM_IOWR(DRM_COMMAND_BASE + DRM_I915_DPST_CONTEXT, \
+		struct dpst_initialize_context)
 
 /* Allow drivers to submit batchbuffers directly to hardware, relying
  * on the security mechanisms provided by hardware.
@@ -388,6 +392,10 @@ typedef struct drm_i915_irq_wait {
 #define I915_PARAM_HAS_EXEC_HANDLE_LUT   26
 #define I915_PARAM_HAS_WT     	 	 27
 #define I915_PARAM_CMD_PARSER_VERSION	 28
+
+/* Private (not upstreamed) parameters start from 96      */
+/* This helps to avoid conflicts with new upstream values */
+#define I915_PARAM_DPST_ACTIVE		 96 
 
 typedef struct drm_i915_getparam {
 	int param;
@@ -1133,5 +1141,71 @@ struct drm_i915_gem_userptr {
 struct drm_i915_reserved_reg_bit_2 {
 	__u32 enable;
 	int plane;
+};
+
+#define	DPST_DIET_ENTRY_COUNT	33	/* Total number of DIET entries */
+#define DPST_RESET_IE		0x40004000
+#define DPST_MAX_FACTOR		100
+struct dpst_ie {
+	enum dpst_diet_alg {
+		i915_DPST_RGB_TRANSLATOR = 0,
+		i915_DPST_YUV_ADDER,
+		i915_DPST_HSV_MULTIPLIER
+	} diet_algorithm;
+	__u32  base_lut_index;	/* Base lut index (192 for legacy mode)*/
+	__u32  factor_present[DPST_DIET_ENTRY_COUNT];
+	__u32  factor_new[DPST_DIET_ENTRY_COUNT];
+	__u32  factor_scalar;
+};
+
+struct dpst_ie_container {
+	struct dpst_ie dpst_ie_st;
+	__u32	dpst_blc_freq;
+	__u32	dpst_blc_factor;
+	__u32	pipe_n;
+	__u32	reset_int;
+};
+
+struct dpst_initialize_data {
+	__u32 pipe_n;
+	__u32 threshold_gb;
+	__u32 gb_delay;
+	__u32 hist_reg_values;
+	__u32 blc_inv_settings;
+	__u32 sig_num;
+};
+
+struct dpst_histogram {
+	__u16	event;
+	__u32	status[32];
+	__u32	threshold[12];
+	__u32	gb_val;
+	__u32	gb_int_delay;
+	__u32   bkl_val;
+	enum dpst_hist_mode {
+		i915_DPST_YUV_LUMA_MODE = 0,
+		i915_DPST_HSV_INTENSITY_MODE
+	} hist_mode;
+};
+
+struct dpst_histogram_status {
+	__u32	pipe_n;
+	struct dpst_histogram histogram_bins;
+};
+
+struct dpst_initialize_context {
+	enum dpst_call_type {
+		DPST_ENABLE = 1,
+		DPST_DISABLE,
+		DPST_INIT_DATA,
+		DPST_GET_BIN_DATA,
+		DPST_APPLY_LUMA,
+		DPST_RESET_HISTOGRAM_STATUS
+	} dpst_ioctl_type;
+	union {
+		struct dpst_initialize_data	init_data;
+		struct dpst_ie_container	ie_container;
+		struct dpst_histogram_status	hist_status;
+	};
 };
 #endif /* _I915_DRM_H_ */
