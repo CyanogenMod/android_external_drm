e47db72e176cbc6e98bf958f0ddd61e0ac9e2094
Adam Rutkowski <adam.j.rutkowski@intel.com>
2014-02-12 10:56:22 +0100

    FOR_UPSTREAM [VPG]: drm/i915: Perfmon IOCTL definitions
    Perfmon IOCTL definitions for usermode.
    
    Issue: VIZ-3542
    Change-Id: Ibdee80dab482a606d6953ab410ad5a816f5fc439
    Depends-Change-Id: Ie207e31d685625c1bc9cd6c7b2668eb5c9d0fd8c
    Signed-off-by: Adam Rutkowski <adam.j.rutkowski@intel.com>
    Reviewed-on: https://android.intel.com/162919
    Reviewed-by: Madajczak, Tomasz <Tomasz.Madajczak@intel.com>
    Reviewed-by: Nordell, Joakim <joakim.nordell@intel.com>
    Reviewed-by: Bloomfield, Jon <jon.bloomfield@intel.com>
    Tested-by: Madajczak, Tomasz <Tomasz.Madajczak@intel.com>

--- include/drm/i915_drm.h
+++ include/drm/i915_drm.h
@@ -28,6 +28,7 @@
 #define _I915_DRM_H_
 
 #include <drm.h>
+#include <i915_perfmon.h>
 
 /* Please note that modifications to all structs defined here are
  * subject to backwards-compatibility constraints.
@@ -265,6 +266,7 @@ typedef struct _drm_i915_sarea {
 #define DRM_I915_SET_CSC                0x39
 #define DRM_I915_DPST_CONTEXT		0x3b
 #define DRM_I915_GEM_ACCESS_USERDATA	0x3c
+#define DRM_I915_PERFMON		0x3e
 
 #define DRM_IOCTL_I915_INIT		DRM_IOW( DRM_COMMAND_BASE + DRM_I915_INIT, drm_i915_init_t)
 #define DRM_IOCTL_I915_FLUSH		DRM_IO ( DRM_COMMAND_BASE + DRM_I915_FLUSH)
@@ -327,6 +329,8 @@ typedef struct _drm_i915_sarea {
 #define DRM_IOCTL_I915_DPST_CONTEXT			\
 		DRM_IOWR(DRM_COMMAND_BASE + DRM_I915_DPST_CONTEXT, \
 		struct dpst_initialize_context)
+#define DRM_IOCTL_I915_PERFMON DRM_IOWR(DRM_COMMAND_BASE + DRM_I915_PERFMON, \
+		struct drm_i915_perfmon)
 
 /* Allow drivers to submit batchbuffers directly to hardware, relying
  * on the security mechanisms provided by hardware.
